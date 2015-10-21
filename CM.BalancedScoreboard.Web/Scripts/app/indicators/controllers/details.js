d5e224cd5b752ef4bde9660c11b220b10adcc0a2
Roberto <rob.garcia.martinez@gmail.com>
2015-10-21 19:23:44 +0100

    Indicator values table enhanced

--- CM.BalancedScoreboard.Web/Scripts/app/indicators/controllers/details.js
+++ CM.BalancedScoreboard.Web/Scripts/app/indicators/controllers/details.js
@@ -1,55 +1,60 @@
 ﻿indicatorsApp.controller('indicatorsDetailsCtrl', function ($scope, $routeParams, indicatorsApi, $filter, ngTableParams) {
     $scope.submitIndicator = function () {
+        $scope.indicator.StartDate = $scope.startDate;
         $scope.indicator.ComparisonValueType = $scope.selectedComparisonValue.id;
         $scope.indicator.PeriodicityType = $scope.selectedPeriodicity.id;
         $scope.indicator.ObjectValueType = $scope.selectedObjectValue.id;
         indicatorsApi.update({ id: $scope.indicator.Id }, $scope.indicator);
     }
 
+    $scope.formatDate = function (date) {
+        var d = new Date(date),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear();
+
+        if (month.length < 2) month = '0' + month;
+        if (day.length < 2) day = '0' + day;
+
+        return [year, month, day].join('/');
+    };
+
     function init() {
         indicatorsApi.get({ id: $routeParams.indicatorId }).$promise
             .then(function (data) {
-                $scope.indicator = data.Indicator;
-                $scope.comparisonValueTypeList = data.ComparisonValueTypeList;
-                $scope.periodicityTypeList = data.PeriodicityTypeList;
-                $scope.objectValueTypeList = data.ObjectValueTypeList;
-                $scope.splitTypeList = data.SplitTypeList;
-                $scope.selectedComparisonValue = $.grep($scope.comparisonValueTypeList, function (e) { return e.id === data.Indicator.ComparisonValueType; })[0];
-                $scope.selectedPeriodicity = $.grep($scope.periodicityTypeList, function (e) { return e.id === data.Indicator.PeriodicityType; })[0];
-                $scope.selectedObjectValue = $.grep($scope.objectValueTypeList, function (e) { return e.id === data.Indicator.ObjectValueType; })[0];
-                
+                assignValues(data);
             })
             .catch(function (msg) {
                 console.error(msg);
             });
     };
 
-    //this.tableParams = new NgTableParams({}, {
-    //    getData: function (params) {
-    //        params.total($scope.indicator.Values.Count());
-    //        return $scope.indicator.Values;
-    //    }
-    //});
+    function assignValues(data) {
+        $scope.indicator = data.Indicator;
+        $scope.comparisonValueTypeList = data.ComparisonValueTypeList;
+        $scope.periodicityTypeList = data.PeriodicityTypeList;
+        $scope.objectValueTypeList = data.ObjectValueTypeList;
+        $scope.splitTypeList = data.SplitTypeList;
+        $scope.startDate = new Date(data.Indicator.StartDate);
+        $scope.selectedComparisonValue = $.grep($scope.comparisonValueTypeList, function (e) { return e.id === data.Indicator.ComparisonValueType; })[0];
+        $scope.selectedPeriodicity = $.grep($scope.periodicityTypeList, function (e) { return e.id === data.Indicator.PeriodicityType; })[0];
+        $scope.selectedObjectValue = $.grep($scope.objectValueTypeList, function (e) { return e.id === data.Indicator.ObjectValueType; })[0];
+        $scope.tableParams = new ngTableParams(
+        {
+            page: 1,
+            count: 12,
+            sorting: {
+                Date: 'desc'
+            }
+        },
+        {
+            total: data.Count,
+            counts: [],
+            getData: function ($defer, params) {
+                $defer.resolve($filter('orderBy')(data.Indicator.Values, params.orderBy()));
+            }
+        });
+    };
 
     init();
-
-    var data = [
-    { Id:1, Date: "2015-06-01", RecordValue: "50", TargetValue: "50" },
-    { Id:2, Date: "2015-07-01", RecordValue: "43", TargetValue: "50" }
-    ];
-
-    $scope.tableParams = new ngTableParams(
-    {
-        page: 1,
-        count: 12,
-        sorting: {
-            RecordValue: 'asc'
-        }
-    },
-    {
-        total: data.Count,
-        getData: function ($defer, params) {
-            $defer.resolve($scope.data);
-        }
-    });
 });
