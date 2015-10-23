bab4e3177ada77727210bac2425ab87053c73233
Roberto <rob.garcia.martinez@gmail.com>
2015-10-23 22:02:46 +0100

    Style crappy changes

---
+++ CM.BalancedScoreboard.Web/Scripts/app/indicators/directives/card/card.js
@@ -0,0 +1,10 @@
+﻿var myIndicatorCard = indicatorsApp.directive('myIndicatorCard', function () {
+    return {
+        scope: {
+            indicator: '='
+        },
+        restrict: 'E',
+        templateUrl: '/Scripts/app/indicators/directives/card/views/view.html',
+        controller: myIndicatorCardController
+    }
+});
