b6251473a3de8127153c3a78ad3cd2075f661862
Roberto Garcia <r.garcia@syzygy.co.uk>
2015-12-11 16:19:19 +0000

    Updates to fix the 404 when reloading the page

--- CM.BalancedScoreboard.Web/app/main.js
+++ CM.BalancedScoreboard.Web/app/main.js
@@ -5,24 +5,28 @@ angular.module("app", ["ngRoute", "indicators", "projects"])
     .config([
         "$routeProvider", "$locationProvider",
         function ($routeProvider, $locationProvider) {
-            $routeProvider.
-                when("/Indicators/List", {
+            $routeProvider
+                .when("/Indicators/List", {
                     templateUrl: "app/indicators/views/list.html",
                     controller: "indicatorsListCtrl"
-                }).
-                when("/Indicators/Details/", {
+                })
+                .when("/Indicators/Details/", {
                     templateUrl: "app/indicators/views/details.html",
                     controller: "indicatorsDetailsCtrl"
-                }).
-                when("/Indicators/Details/:indicatorId", {
+                })
+                .when("/Indicators/Add/", {
                     templateUrl: "app/indicators/views/details.html",
                     controller: "indicatorsDetailsCtrl"
-                }).
-                when("/Projects/List", {
+                })
+                .when("/Indicators/Details/:indicatorId", {
+                    templateUrl: "app/indicators/views/details.html",
+                    controller: "indicatorsDetailsCtrl"
+                })
+                .when("/Projects/List", {
                     templateUrl: "app/projects/views/list.html",
                     controller: "projectsListCtrl"
-                }).
-                otherwise({
+                })
+                .otherwise({
                     redirectTo: "/Indicators/List"
                 });
             $locationProvider.html5Mode(true)
